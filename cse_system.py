2142f0b63591fdd6a5bb919d1b8e51ab257e28e2
SheshamJoseph <sheshamabraham@gmail.com>
2024-11-16 15:50:05 +0100

    added docstring for cse_system

--- cse_system.py
+++ cse_system.py
@@ -2,17 +2,35 @@ from matplotlib import axis
 import pandas as pd
 
 class CSESystem:
+    """
+    A system that combines a rule-based engine and a machine learning model to detect potentially suspicious messages.
+    
+    The rule engine acts as a filter to quickly assess whether a message is suspicious. If the rule engine flags a
+    message as suspicious, it is then passed to a machine learning model for further analysis. The model provides
+    a more detailed classification, with results indicating whether a message is suspicious ('yes') or not ('no').
+    """
     def __init__(self, model, engine):
         """
-            
+        Initializes the SuspiciousMessageSystem with a rule-based engine and a machine learning model.
         Args:
-            model (_type_): _description_
-            engine (_type_): _description_
+            model (object): A trained machine learning model that returns an array with
+                            probabilities or class predictions.
+            engine (object): Instance of rule based engine
         """
         self.model = model
         self.engine = engine
         
     def predict(self, messages):
+        """
+        Predicts whether each message in a given list or pd.Series is suspicious
+        It first passes each message to the rule engine, if the message is flagged as suspicous
+        it is then passed to the model for deeper analysis
+        Args:
+            messages (str | list | pd.Series)
+
+        Returns:
+            list[str]: A list of labels('yes' or 'no') for each messgae
+        """
         # Ensure input can be processed as a list of messages
         if isinstance(messages, str):
             messages = [messages]
